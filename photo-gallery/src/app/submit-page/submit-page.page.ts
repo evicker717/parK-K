016c7aeec0fcbfcbd2cc16e289f27821eff8f137
Evan Vicker <evan.vicker19@kzoo.edu>
2022-10-03 10:12:20 -0400

    added submition page

---
+++ photo-gallery/src/app/submit-page/submit-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-page',
+  templateUrl: './submit-page.page.html',
+  styleUrls: ['./submit-page.page.scss'],
+})
+export class SubmitPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
