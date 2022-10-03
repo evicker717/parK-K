0a9bacace22a6569775aab60966e6de6bbd922a3
Evan Vicker <evan.vicker19@kzoo.edu>
2022-10-03 11:35:29 -0400

    added submition button that thanks user for submiting and rediredirects to main page

--- photo-gallery/src/app/submit-page/submit-page.page.ts
+++ photo-gallery/src/app/submit-page/submit-page.page.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-submit-page',
   templateUrl: './submit-page.page.html',
   styleUrls: ['./submit-page.page.scss'],
 })
 export class SubmitPagePage implements OnInit {
+  handlerMessage = '';
+  roleMessage = '';
+  constructor(private alertController: AlertController, private router:Router){}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Thank you for helping us provide accurate data!',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            this.router.navigate(['/tabs/tab1'])
+          },
+        },
+      ],
+    });
 
-  constructor() { }
+    await alert.present();
 
+    const { role } = await alert.onDidDismiss();
+    this.roleMessage = `Dismissed with role: ${role}`;
+  }
   ngOnInit() {
   }
 
